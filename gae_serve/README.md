b3e8b8a70585b44593ea30cc388b6890996f15f8
Yu-Han Liu <yuhanliu@google.com>
2017-09-06 12:27:36 -0700

    first commit

---
+++ gae_serve/README.md
@@ -0,0 +1,21 @@
+
+## Introduction
+
+This is a simple sample that shows how to:
+
+- Create a GAE service that loads a pickled sci-kit learn model from GCS, and use it to serve prediction requests through Cloud Endpoints.
+
+
+[comment]: #(get Andrew to help set these up as the advanced usage section)
+The benefits of this configuration include:
+
+1. GAE's autoscaling and load balancing.
+1. Cloud Endpoints' monitoring and access control.
+
+
+## Requirements
+
+A GCP project that has GAE, GCS, and Cloud Endpoints enabled.
+
+
+## 
---
+++ gae_serve/main.py
@@ -0,0 +1,65 @@
+# Copyright 2017 Google Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# [START app]
+import json
+import logging
+import os
+
+import pickle
+from google.cloud import storage
+from flask import Flask, request
+
+MODEL_BUCKET = os.environ['MODEL_BUCKET']
+MODEL_FILENAME = os.environ['MODEL_FILENAME']
+MODEL = None
+
+app = Flask(__name__)
+
+_CURL = """
+curl -H "Content-Type: application/json" -X POST -d '{"X": [[1, 2], [5, -1], [1, 0]]}' "https://skserve-dot-rising-sea-112358.appspot.com/predict?key=AIzaSyAmTTe0NktLMOTaqTY7G1v1yNw1SaTyXuw"
+"""
+
+# NOTE: the first response could be slower
+@app.before_first_request
+def _load_model():
+    global MODEL
+    client = storage.Client()
+    bucket = client.get_bucket(MODEL_BUCKET)
+    blob = bucket.get_blob(MODEL_FILENAME)
+    s = blob.download_as_string()
+
+    MODEL = pickle.loads(s)
+
+@app.route('/', methods=['GET'])
+def index():
+    return str(MODEL), 200
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    X = request.get_json()['X']
+    y = MODEL.predict(X).tolist()
+    return json.dumps({'y': y}), 200
+
+@app.errorhandler(500)
+def server_error(e):
+    logging.exception('An error occurred during a request.')
+    return """
+    An internal error occurred: <pre>{}</pre>
+    See logs for full stacktrace.
+    """.format(e), 500
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=8080, debug=True)
+# [END app]
