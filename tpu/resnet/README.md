7f6ff694134504f75137d9b99edefc32957e76f4
Yu-Han Liu <yuhanliu@google.com>
2018-06-14 12:59:14 -0700

    add tpu/resnet:r1.8

---
+++ tpu/resnet/README.md
@@ -0,0 +1,3 @@
+ResNet training on CMLE with TPUs and hyperparameter tuning.
+
+Adapted from https://github.com/tensorflow/tpu/tree/master/models/official/resnet
---
+++ tpu/resnet/submit_resnet.sh
@@ -0,0 +1,24 @@
+
+now=$(date +"%Y%m%d_%H%M%S")
+BUCKET="gs://sandbox-tpu"
+
+JOB_NAME="tpu_$now"
+JOB_DIR=$BUCKET"/"$JOB_NAME
+
+STAGING_BUCKET=$BUCKET
+REGION=us-central1
+DATA_DIR=gs://cloud-tpu-test-datasets/fake_imagenet
+OUTPUT_PATH=$JOB_DIR
+
+gcloud ml-engine jobs submit training $JOB_NAME \
+    --staging-bucket $STAGING_BUCKET \
+    --runtime-version 1.8 \
+    --module-name resnet.resnet_main \
+    --package-path resnet/ \
+    --region $REGION \
+    --config config_resnet.yaml \
+    -- \
+    --data_dir=$DATA_DIR \
+    --model_dir=$OUTPUT_PATH \
+    --export_dir=$OUTPUT_PATH \
+    --resnet_depth=101
