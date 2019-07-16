41ddf25e7e4d889f1ef937c13f59f7a487557572
Gonzalo Gasca Meza <gogasca@google.com>
2019-07-16 12:27:51 -0700

    Update convert_to_rt.py

--- dlvm/tools/convert_to_rt.py
+++ dlvm/tools/convert_to_rt.py
@@ -22,12 +22,13 @@ parser = argparse.ArgumentParser(
 parser.add_argument("--input_model_dir", required=True)
 parser.add_argument("--output_model_dir", required=True)
 parser.add_argument("--batch_size", required=True, type=int)
-parser.add_argument("--precision_mode", required=True,
-										choices=["FP32", "FP16", "INT8", "INT4"])
+parser.add_argument("--precision_mode", required=True, choices=["FP32", "FP16", "INT8", "INT4"])
 args = parser.parse_args()
 
 trt.create_inference_graph(
-	None, None, max_batch_size=args.batch_size,
+	None, 
+	None, 
+	max_batch_size=args.batch_size,
 	input_saved_model_dir=args.input_model_dir,
 	output_saved_model_dir=args.output_model_dir,
 	precision_mode=args.precision_mode)
