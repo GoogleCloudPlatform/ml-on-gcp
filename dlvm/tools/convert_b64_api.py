3991f921d8d5b490d943a547d4e9ce8b527bbc6d
Gonzalo Gasca Meza <gogasca@google.com>
2019-08-08 12:38:29 -0700

    Update convert_b64_api.py

--- dlvm/tools/convert_b64_api.py
+++ dlvm/tools/convert_b64_api.py
@@ -7,12 +7,12 @@ OUTPUT_FILE = 'image_b64.json'
 
 def convert_to_base64(image_file):
   """Open image and convert it to base64"""
-  with open(image_file, 'rb') as f:
-    jpeg_bytes = base64.b64encode(f.read()).decode('utf-8')
+  with open(image_file, 'rb') as input_file:
+    jpeg_bytes = base64.b64encode(input_file.read()).decode('utf-8')
     predict_request = '{"instances" : [{"b64": "%s"}]}' % jpeg_bytes
     # Write JSON to file
-    with open(OUTPUT_FILE, 'w') as f:
-      f.write(predict_request)
+    with open(OUTPUT_FILE, 'w') as output_file:
+      output_file.write(predict_request)
     return predict_request
 
 convert_to_base64(INPUT_FILE)
