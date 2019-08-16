6112b76d90a805906e7680ee1a61d4fee0e9cb53
Gonzalo Gasca Meza <gogasca@google.com>
2019-08-16 16:21:32 -0700

    ML Flow tutorial in GCP

---
+++ tutorials/tensorflow/databricks_mlflow/trainer/model_deployment.py
@@ -0,0 +1,162 @@
+# Copyright 2019 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Deploy a model in AI Platform."""
+
+import logging
+import json
+import time
+from googleapiclient import discovery
+from googleapiclient import errors
+
+_WAIT_FOR_COMPLETION_SLEEP_SECONDS = 10
+RUN_TIME_VERSION = '1.14'
+
+
+def _create_service():
+    """Gets service instance to start API searches.
+
+    :return:
+    """
+    return discovery.build('ml', 'v1')
+
+
+class AIPlatformModel(object):
+    def __init__(self, project_id):
+        self._project_id = project_id
+        self._service = _create_service()
+
+    def model_exists(self, model_name):
+        """
+
+        :param model_name:
+        :return:
+        """
+        models = self._service.projects().models()
+        try:
+            response = models.list(
+                parent='projects/{}'.format(self._project_id)).execute()
+            if response:
+                for model in response['models']:
+                    if model['name'].rsplit('/', 1)[1] == model_name:
+                        return True
+                    else:
+                        return False
+        except errors.HttpError as err:
+            logging.error('%s', json.loads(err.content)['error']['message'])
+
+    def _list_model_versions(self, model_name):
+        """Lists existing model versions in the project.
+
+        Args:
+          model_name: Model name to list versions for.
+
+        Returns:
+          Dictionary of model versions.
+        """
+        versions = self._service.projects().models().versions()
+        request = versions.list(
+            parent='projects/{}/models/{}'.format(self._project_id, model_name))
+        try:
+            return request.execute()
+        except errors.HttpError as err:
+            logging.error('%s', json.loads(err.content)['error']['message'])
+
+    def create_model(self, model_name, model_region='us-central1'):
+        """
+
+        :param model_name:
+        :param model_region:
+        :return:
+        """
+        if not self.model_exists(model_name):
+            body = {
+                'name': model_name,
+                'regions': model_region,
+                'description': 'MLflow model'
+            }
+            parent = 'projects/{}'.format(self._project_id)
+            try:
+                self._service.projects().models().create(
+                    parent=parent, body=body).execute()
+                logging.info('Model "%s" has been created.', model_name)
+            except errors.HttpError as err:
+                logging.error('"%s". Skipping model creation.',
+                              json.loads(err.content)['error']['message'])
+        else:
+            logging.warning('Model "%s" already exists.', model_name)
+
+    def deploy_model(self, bucket_name, model_name, model_version,
+                     runtime_version=RUN_TIME_VERSION):
+        """Deploys model on AI Platform.
+
+        Args:
+          bucket_name: Cloud Storage Bucket name that stores saved model.
+          model_name: Model name to deploy.
+          model_version: Model version.
+          runtime_version: Runtime version.
+
+        Raises:
+          RuntimeError if deployment completes with errors.
+        """
+        # For details on request body, refer to:
+        # https://cloud.google.com/ml-engine/reference/rest/v1/projects
+        # .models.versions/create
+        model_version_exists = False
+        model_versions_list = self._list_model_versions(model_name)
+        #  Field: version.name Error: A name should start with a letter and
+        #  contain only letters, numbers and underscores
+        model_version = 'mlflow_{}'.format(model_version)
+
+        if model_versions_list:
+            for version in model_versions_list['versions']:
+                if version['name'].rsplit('/', 1)[1] == model_version:
+                    model_version_exists = True
+
+        if not model_version_exists:
+            request_body = {
+                'name': model_version,
+                'deploymentUri': '{}'.format(bucket_name),
+                'framework': 'TENSORFLOW',
+                'runtimeVersion': runtime_version
+            }
+            parent = 'projects/{}/models/{}'.format(self._project_id,
+                                                    model_name)
+            response = self._service.projects().models().versions().create(
+                parent=parent, body=request_body).execute()
+            op_name = response['name']
+            while True:
+                deploy_status = (
+                    self._service.projects().operations().get(
+                        name=op_name).execute())
+                if deploy_status.get('done'):
+                    logging.info('Model "%s" with version "%s" deployed.',
+                                 model_name,
+                                 model_version)
+                    break
+                if deploy_status.get('error'):
+                    logging.error(deploy_status['error'])
+                    raise RuntimeError(
+                        'Failed to deploy model for serving: {}'.format(
+                            deploy_status['error']))
+                logging.info(
+                    'Waiting for %d seconds for "%s" with "%s" version to be '
+                    'deployed.',
+                    _WAIT_FOR_COMPLETION_SLEEP_SECONDS, model_name,
+                    model_version)
+                time.sleep(_WAIT_FOR_COMPLETION_SLEEP_SECONDS)
+
+        else:
+            logging.info('Model "%s" with version "%s" already exists.',
+                         model_name,
+                         model_version)
---
+++ tutorials/tensorflow/databricks_mlflow/trainer/task.py
@@ -0,0 +1,257 @@
+# Copyright 2019 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Trains a Keras model to predict income bracket from other Census data."""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import argparse
+import subprocess
+import logging
+import os
+
+from builtins import int
+from mlflow import pyfunc
+from tensorflow.python.saved_model import tag_constants
+from time import time
+
+from . import model
+from . import utils
+from . import model_deployment
+
+import mlflow
+import mlflow.tensorflow
+import tensorflow as tf
+
+mlflow.tensorflow.autolog()
+
+
+def get_args():
+    """Argument parser.
+
+    Returns:
+      Dictionary of arguments.
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--job-dir',
+        type=str,
+        required=True,
+        help='Local or GCS location for writing checkpoints and exporting '
+             'models')
+    parser.add_argument(
+        '--num-epochs',
+        type=int,
+        default=20,
+        help='number of times to go through the data, default=20')
+    parser.add_argument(
+        '--batch-size',
+        default=128,
+        type=int,
+        help='number of records to read during each training step, default=128')
+    parser.add_argument(
+        '--learning-rate',
+        default=.01,
+        type=float,
+        help='learning rate for gradient descent, default=.01')
+    parser.add_argument(
+        '--eval-steps',
+        help='Number of steps to run evaluation for, at each checkpoint',
+        default=1,
+        type=int)
+    parser.add_argument(
+        '--reuse-job-dir',
+        action='store_true',
+        default=False,
+        help="""
+          Flag to decide if the model checkpoint should be re-used from the 
+          job-dir.
+          If set to False then the job-dir will be deleted.
+          """)
+    parser.add_argument(
+        '--verbosity',
+        choices=['DEBUG', 'ERROR', 'FATAL', 'INFO', 'WARN'],
+        default='INFO')
+    parser.add_argument(
+        '--deploy-gcp',
+        action='store_true',
+        default=False,
+        help='local or GCS location for writing checkpoints and exporting '
+             'models')
+    parser.add_argument(
+        '--project-id',
+        type=str,
+        help='AI Platform project id')
+    parser.add_argument(
+        '--gcs-bucket',
+        type=str,
+        default='mlflow_gcp',
+        help='AI Platform GCS bucket')
+    parser.add_argument(
+        '--model-name',
+        type=str,
+        default='mlflow',
+        help='AI Platform model')
+    parser.add_argument(
+        '--run-time-version',
+        type=str,
+        default='1.14',
+        help='AI Platform Run time version')
+    args, _ = parser.parse_known_args()
+    return args
+
+
+def _log_metrics(metrics, metric_name):
+    """
+
+    :param metrics:
+    :param metric_name:
+    :return:
+    """
+    for epoch, metric in enumerate(metrics[metric_name], 1): mlflow.log_metric(
+        metric_name, metric,
+        step=epoch)
+
+
+def train_and_evaluate(args):
+    """Trains and evaluates the Keras model.
+
+    Uses the Keras model defined in model.py and trains on data loaded and
+    preprocessed in utils.py. Saves the trained model in TensorFlow SavedModel
+    format to the path defined in part by the --job-dir argument.
+
+    History objects returns:
+        {'loss': [0.5699903990809373, 0.3629718415849791],
+         'acc': [0.78604823, 0.8331693],
+         'val_loss': [0.3966572880744934, 0.3477487564086914],
+         'val_acc': [0.8278044, 0.8281116],
+         'lr': [0.02, 0.015]}
+    Args:
+      args: dictionary of arguments - see get_args() for details
+    """
+
+    logging.info('Resume training:', args.reuse_job_dir)
+    if not args.reuse_job_dir:
+        if tf.io.gfile.exists(args.job_dir):
+            tf.io.gfile.rmtree(args.job_dir)
+            logging.info(
+                'Deleted job_dir {} to avoid re-use'.format(args.job_dir))
+    else:
+        logging.info('Reusing job_dir {} if it exists'.format(args.job_dir))
+
+    train_x, train_y, eval_x, eval_y = utils.load_data()
+
+    # dimensions
+    num_train_examples, input_dim = train_x.shape
+    num_eval_examples = eval_x.shape[0]
+
+    # Create the Keras Model
+    keras_model = model.create_keras_model(
+        input_dim=input_dim, learning_rate=args.learning_rate)
+
+    # Pass a numpy array by passing DataFrame.values
+    training_dataset = model.input_fn(
+        features=train_x.values,
+        labels=train_y,
+        shuffle=True,
+        num_epochs=args.num_epochs,
+        batch_size=args.batch_size)
+
+    # Pass a numpy array by passing DataFrame.values
+    validation_dataset = model.input_fn(
+        features=eval_x.values,
+        labels=eval_y,
+        shuffle=False,
+        num_epochs=args.num_epochs,
+        batch_size=num_eval_examples)
+
+    start_time = time()
+    # Train model
+
+    with mlflow.start_run() as active_run:
+        run_id = active_run.info.run_id
+        # Setup Learning Rate decay.
+        lr_decay_callback = tf.keras.callbacks.LearningRateScheduler(
+            lambda epoch: args.learning_rate + 0.02 * (0.5 ** (1 + epoch)),
+            verbose=False)
+        # Setup TensorBoard callback.
+        tensorboard_callback = tf.keras.callbacks.TensorBoard(
+            os.path.join(args.job_dir, run_id, 'tensorboard'),
+            histogram_freq=1)
+        history = keras_model.fit(
+            training_dataset,
+            steps_per_epoch=int(num_train_examples / args.batch_size),
+            epochs=args.num_epochs,
+            validation_data=validation_dataset,
+            validation_steps=args.eval_steps,
+            verbose=1,
+            callbacks=[lr_decay_callback, tensorboard_callback])
+        metrics = history.history
+        logging.info(metrics)
+        keras_model.summary()
+        # Export SavedModel
+        model_export_path = os.path.join(args.job_dir, run_id, 'model')
+        tf.keras.experimental.export_saved_model(keras_model, model_export_path)
+        # Define artifacts.
+        logging.info('Model exported to: ', model_export_path)
+        mlflow.tensorflow.log_model(tf_saved_model_dir=model_export_path,
+                                    tf_meta_graph_tags=[tag_constants.SERVING],
+                                    tf_signature_def_key='serving_default',
+                                    artifact_path='model')
+        mlflow.log_param('num_epochs', args.num_epochs)
+        mlflow.log_param('batch_size', args.batch_size)
+        mlflow.log_param('learning_rate', args.learning_rate)
+        mlflow.log_param('train_samples', num_train_examples)
+        mlflow.log_param('eval_samples', num_eval_examples)
+        mlflow.log_param('eval_steps', args.eval_steps)
+        mlflow.log_param('steps_per_epoch',
+                         int(num_train_examples / args.batch_size))
+        # Add metrics
+        _log_metrics(metrics, 'loss')
+        _log_metrics(metrics, 'acc')
+        _log_metrics(metrics, 'val_loss')
+        _log_metrics(metrics, 'val_acc')
+        _log_metrics(metrics, 'lr')
+        # Reloading the model
+        pyfunc_model = mlflow.pyfunc.load_model(
+            mlflow.get_artifact_uri('model'))
+        logging.info('Uploading TensorFlow events as a run artifact.')
+        timed = time() - start_time
+        mlflow.log_metric("time", timed)
+        mlflow.end_run()
+
+    # Deploy to AI Platform.
+    if args.deploy_gcp:
+        # Create AI Platform helper instance.
+        model_helper = model_deployment.AIPlatformModel(
+            project_id=args.project_id)
+        # Copy local model to GCS for deployment.
+        local_path = os.path.join(model_export_path)
+        gcs_path = os.path.join('gs://', args.gcs_bucket, run_id, 'model')
+        subprocess.call(
+            "gsutil -m cp -r {} {}".format(local_path, gcs_path), shell=True)
+        # Create model
+        model_helper.create_model(args.model_name)
+        # Create model version
+        model_helper.deploy_model(gcs_path, args.model_name, run_id,
+                                  args.run_time_version)
+        print('Model deployment in GCP completed')
+    print("This model took", timed, "seconds to train and test.")
+
+
+if __name__ == '__main__':
+    args = get_args()
+    tf.compat.v1.logging.set_verbosity(args.verbosity)
+    train_and_evaluate(args)
